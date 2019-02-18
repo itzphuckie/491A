84ce9e4744a7141a6c7a239ea61191b7bf82a89c
promo230 <35945142+promo230@users.noreply.github.com>
2019-02-18 09:00:10 -0800

    Update MainActivity.java

--- app/src/main/java/com/example/owner/ppkrinc/MainActivity.java
+++ app/src/main/java/com/example/owner/ppkrinc/MainActivity.java
@@ -6,36 +6,47 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private String userID;
+    private Bundle loginData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        loginData = getIntent().getExtras();
+        userID = loginData.getString("UserID");
+
     }
 
     public void launchPostParkingSpotActivity(View view){
         Intent intent = new Intent(getApplicationContext(), PostParkingSpotActivity.class);
+        intent.putExtras(loginData);
         startActivity(intent);
     }
 
     public void launchSearchParkingSpotAct(View view) {
         Intent intent = new Intent(getApplicationContext(), SearchParking.class);
+        intent.putExtras(loginData);
         startActivity(intent);
     }
 
     public void launchRecentMatchesAct(View view) {
         Intent intent = new Intent(getApplicationContext(), PostParkingSpotActivity.class);
+        intent.putExtras(loginData);
         startActivity(intent);
     }
 
     public void launchMyAccountAct(View view) {
         Intent intent = new Intent(getApplicationContext(), MyAccount.class);
+        intent.putExtras(loginData);
         startActivity(intent);
     }
 
     public void launchAboutUsAct(View view) {
 
         Intent intent = new Intent(getApplicationContext(), AboutUs.class);
+        intent.putExtras(loginData);
         startActivity(intent);
     }
 }
